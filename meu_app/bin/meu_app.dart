d5b5b536c7004bb79f6b4fe890004b760573abd8
JaoMcs <joaort.azul@gmail.com>
2023-09-12 13:06:23 -0300

    Testes com a linguagem DART

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,5 +1,33 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  print('Hello world - ${meu_app.calculate()}!');
+  // print('Hello world - ${meu_app.calculate()}!');
+
+  var varTrue = true;
+  bool varFalse = false;
+
+  print(varFalse);
+  print(varTrue);
+
+  var string = "StringQualquer";
+  String string2 = 'b';
+
+  print(string);
+  print(string2);
+
+  List<String> nomes = ["Joao", "Maria"];
+  print(nomes);
+
+  nomes.add("Robertinho");
+  print(nomes);
+
+  List listaDinamica = [];
+  listaDinamica.add("Valor");
+  listaDinamica.add(2);
+  listaDinamica.add(2.5);
+  print(listaDinamica);
+  print(listaDinamica.isEmpty);
+
+  Map<String, dynamic> map1 = {'nome': 'João', 'idade': 23};
+  print(map1);
 }
