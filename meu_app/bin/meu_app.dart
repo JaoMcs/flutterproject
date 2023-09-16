3a3729f26af6c31cbe916a6188aede286614516b
JaoMcs <joaort.azul@gmail.com>
2023-09-16 11:07:31 -0300

    calculadora de IMC

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,6 +1,88 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
+import 'dart:io';
 
 void main(List<String> arguments) {
+  String nome;
+  double peso;
+  double altura;
+
+  print("Informe os dados de pessoa:");
+
+  print("Qual a seu nome?");
+  String? nomeInput = stdin.readLineSync();
+  nome = ifLetDados(nomeInput).toString();
+
+  print("Qual a sua peso?");
+  String? pesoImput = stdin.readLineSync();
+  peso = double.parse(ifLetDados(pesoImput));
+
+  print("Qual a sua altura?");
+  String? alturaInput = stdin.readLineSync();
+  altura = double.parse(ifLetDados(alturaInput));
+
+  var pessoa = Pessoa(nome, peso, altura);
+  calculaIMC(pessoa);
+}
+
+void calculaIMC(Pessoa pessoa) {
+  print("Calculando o IMC da pessoa: ");
+  print(pessoa.nome);
+  var altura = pessoa.altura * pessoa.altura;
+  var valor = pessoa.peso / altura;
+
+  var IMC = valorToIMC(valor);
+  print(valor);
+  print(IMC);
+}
+
+dynamic ifLetDados(dynamic isData) {
+  if (isData != null) {
+    return isData;
+  } else {
+    print("Dado invalido");
+  }
+}
+
+class Pessoa {
+  String nome;
+  double peso;
+  double altura;
+
+  Pessoa(this.nome, this.peso, this.altura);
+}
+
+enum IMC {
+  magrezaGrave,
+  magrezaModerada,
+  magrezaLeve,
+  saudavel,
+  sobrepeso,
+  obesidade1,
+  obesidade2,
+  obesidade3
+}
+
+IMC valorToIMC(double valor) {
+  if (valor < 16) {
+    return IMC.magrezaGrave;
+  } else if ((valor >= 16) && (valor < 17)) {
+    return IMC.magrezaModerada;
+  } else if ((valor >= 17) && (valor < 18.5)) {
+    return IMC.magrezaLeve;
+  } else if ((valor >= 18.5) && (valor < 25)) {
+    return IMC.saudavel;
+  } else if ((valor >= 25) && (valor < 30)) {
+    return IMC.sobrepeso;
+  } else if ((valor >= 30) && (valor < 35)) {
+    return IMC.obesidade1;
+  } else if ((valor >= 35) && (valor < 45)) {
+    return IMC.obesidade2;
+  } else {
+    return IMC.obesidade3;
+  }
+}
+
+void testFunc() {
   // print('Hello world - ${meu_app.calculate()}!');
 
   var varTrue = true;
